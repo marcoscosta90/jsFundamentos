7d1e6263c65c9bee37266efb5fc0733ba9193b4e
marcoscosta90 <marcoscosta.dev@gmail.com>
2021-01-14 15:11:00 -0300

    Template String

--- fundamentos/strings.js
+++ fundamentos/strings.js
@@ -2,4 +2,12 @@ const escola = 'coder'
 
 console.log(escola.charAt(4));
 console.log(escola.charAt(5));
-console.log(escola.charCodeAt(3))
+console.log(escola.charCodeAt(3));
+console.log(escola.indexOf('o'));
+console.log(escola.substring(1));
+console.log(escola.substring(0, 3));
+
+console.log('Escola'.concat(' ').concat(escola).concat('!'))
+console.log(escola.replace('e', '3'))
+console.log('Ana, Maria, Pedro'.split('/,/'))
+
---
+++ templateString.js
@@ -0,0 +1,9 @@
+const nome = "Rebeca"
+const concatenacao = "Ola " + nome + "!"
+
+const template = `ola ${nome}!`
+
+console.log(concatenacao, template)
+
+const up = texto => texto.toUpperCase();
+console.log(`Ei..... ${up('cuidado')}!`)
